1b5b459551022662677c2be87345dbf34ea16a1a
Vlad <metalhead65@mail.ru>
2024-06-02 14:13:54 +0300

    Update

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const caseElement = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+const deleteCard = (event) => {
+  event.target.closest(".places__item").remove();
+};
 
-// @todo: Функция создания карточки
+function createCard(data, onDelete) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const placesImage = cardElement.querySelector(".card__image");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  cardElement.querySelector(".card__title").textContent = data.name;
+  placesImage.src = data.link;
+  placesImage.alt = data.name;
+  deleteButton.addEventListener('click', onDelete)
+  return cardElement;
+}
 
-// @todo: Функция удаления карточки
+const addCard = (data) => {
+  const newCard = createCard(data, deleteCard);
+  caseElement.prepend(newCard);
+};
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach((data) => {
+  addCard(data);
+});
